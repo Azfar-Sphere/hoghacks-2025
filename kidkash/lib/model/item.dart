6ba0774396fc254ed515c216fd8ce901c76c5b98
Kvn7373 <kfaguilarga@ualr.edu>
2025-04-05 09:18:56 -0500

    Front end Pages
    Grocery store page, item details page, checkout page

---
+++ kidkash/lib/model/item.dart
@@ -0,0 +1,11 @@
+class Item {
+  final String name;
+  final String imageUrl;
+  final int price;
+
+  const Item({
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+  });
+}
