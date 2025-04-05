a6e2d938e673ab1eae604d635fb44b281b0f282c
azfar <s.azfar.rahman@gmail.com>
2025-04-04 23:29:01 -0500

    cv demo code

--- backend/detectface.py
+++ backend/detectface.py
@@ -1,6 +1,7 @@
 from deepface import DeepFace
 import cv2
 import json
+import numpy as np
 
 # Load the video 
 capture = cv2.VideoCapture("IMG_6168.MOV")
@@ -14,31 +15,40 @@ while True:
     if not ret:
         break
 
-    cv2.imshow("Debug Frame", frame)
+    frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    # faces = DeepFace.extract_faces(frame, enforce_detection=False)
+    faces = DeepFace.extract_faces(frame, enforce_detection=False)
 
-    # if not faces:
-    #     print("No Faces detected")
-    #     continue
+    if not faces:
+        print("No Faces detected")
+        continue
 
-    # for i, face in enumerate(faces):
-    #     analysis = DeepFace.analyze(face['face'], actions=['age', 'gender','emotion'])
+    for i, face in enumerate(faces):
+        analysis = DeepFace.analyze(face['face'], actions=['age', 'gender','emotion'], enforce_detection=False)
 
-    #     print(f"Age: {analysis[0]['age']}, Gender: {analysis[0]['gender']}, Emotion: {analysis[0]['Emotion']}")
+        print(f"Age: {analysis[0]['age']}, Gender: {analysis[0]['gender']}, Emotion: {analysis[0]['emotion']}")
 
-    #     x,y,w,h = face['facial_area']
-    #     cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-    #     cv2.putText(frame, f"Age: {analysis[0]['age']}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        x = face['facial_area']['x']
+        y = face['facial_area']['y']
+        w = face['facial_area']['w']
+        h = face['facial_area']['h']
+
+
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.putText(frame, f"Age: {analysis[0]['age']}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)      
+
+        emotion_dict = analysis[0]['emotion']
+        dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+
+        cv2.putText(frame, f"Emotion: {dominant_emotion}", (x+150, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
 
         
-    # cv2.imshow("Frame", frame)
+    cv2.imshow("Frame", frame)
 
-    # Optional: Wait for key press to continue to the next frame
-    key = cv2.waitKey(1)  # Wait for 1 ms before continuing
-
-    if key == ord('q'):  # Press 'q' to quit
-        break  # Exit the loop if 'q' is pressed
+    key = cv2.waitKey(1) & 0xFF  
+    if key == ord('q'):
+        break
 
 capture.release()
 cv2.destroyAllWindows()
