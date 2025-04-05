742279ec39811673db0458417dd8cdd57b8d3be5
James-D- Dempsey <jaydemp1207@gmail.com>
2025-04-05 05:32:18 -0500

    updated role_screen

--- kidkash/lib/Widgets/init_state.dart
+++ kidkash/lib/Widgets/init_state.dart
@@ -1,1 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+enum UserRole { parent, kid }
+
+Future<User?> signInAndSetRole(UserRole role) async {
+  final auth = FirebaseAuth.instance;
+  final firestore = FirebaseFirestore.instance;
+
+  final userCredential = await auth.signInAnonymously();
+  final user = userCredential.user;
+  if (user != null) {
+    await firestore.collection('users').doc(user.uid).set({
+      'role': role.name, // "parent" or "kid"
+      'sessionID': null,
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+  }
+
+  return user;
+}
+
+// This function is waiting on a user value from a front end choice.
+//User? user = FirebaseAuth.instance.currentUser; 
+//if (user == null) {
+//  user = await signInAndSetRole(UserRole.parent); // or prompt for role
+//}
 
