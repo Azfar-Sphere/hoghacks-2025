4645a7c414c4e63c66fd42731e313fed1bef3066
azfar <s.azfar.rahman@gmail.com>
2025-04-05 01:00:47 -0500

    added nod detection

--- backend/detectface.py
+++ backend/detectface.py
@@ -4,12 +4,15 @@ import json
 import numpy as np
 
 # Load the video 
-capture = cv2.VideoCapture("IMG_6170.MOV")
+capture = cv2.VideoCapture("IMG_6171.MOV")
 
 frame_skip = 5
 frame_count = 0
 
+y_history = []
+max_history = 5
 
+threshold = 120
 
 # Read the video frame by frame
 while True:
@@ -39,6 +42,18 @@ while True:
         w = face['facial_area']['w']
         h = face['facial_area']['h']
 
+        face_center_y = y+h // 2
+        y_history.append(face_center_y)
+        if len(y_history) > max_history:
+            y_history.pop(0)
+
+        if len(y_history) == max_history:
+            dy = np.diff(y_history)
+            if (np.max(dy) > threshold and np.min(dy) < -120):
+                cv2.putText(frame, "NOD Detected", (x, y + 250), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)      
+
+
+
         cropped_face = frame[y:y+h, x:x+w]
         if cropped_face.shape[0] < 100 or cropped_face.shape[1] < 100:
             cropped_face = cv2.resize(cropped_face, (100, 100))
@@ -46,7 +61,7 @@ while True:
 
         analysis = DeepFace.analyze(cropped_face, actions=['age', 'gender','emotion'], enforce_detection=False)
 
-        print(f"\n\nAnalysis: {analysis} \n\n")
+        # print(f"\n\nAnalysis: {analysis} \n\n")
 
         # print(f"Age: {analysis[0]['age']}, Gender: {analysis[0]['gender']}, Emotion: {analysis[0]['emotion']}")
 
